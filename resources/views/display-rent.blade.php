4bc4722d95f7f7e2d0bfe1a52d3bc53ef5099d50
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-05 15:18:05 +0800

    updated function button of rate and rate driver

--- resources/views/display-rent.blade.php
+++ resources/views/display-rent.blade.php
@@ -92,11 +92,14 @@
 
                         @else
                         <div class="flex justify-end space-x-4">
-                            <button type="button" data-modal-target="rate-{{ $rent->id }}"
+                            @if($rent->service_rate !== NULL)
+                            @else
+                               <button type="button" data-modal-target="rate-{{ $rent->id }}"
                                 data-modal-toggle="rate-{{ $rent->id }}"
                                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center bg-yellow-100 text-yellow-900 rounded-lg mt-4 cursor-pointer">
                                 Rate
                             </button>
+                            @endif
                             <div
                                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center bg-gray-900 text-white rounded-lg mt-4 cursor-not-allowed select-none">
                                 Cancel
@@ -121,7 +124,7 @@
                             </h3>
                             <button type="button"
                                 class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                data-modal-hide="rate-{{ $rent->id }}" id="clear-rating">
+                                data-modal-hide="rate-{{ $rent->id }}" id="clear-rating-{{ $rent->id }}">
                                 <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                     viewBox="0 0 14 14">
                                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
@@ -184,6 +187,8 @@
 
                                 </div>
 
+                                @if ($rent->driver === NULL)
+                                @else
                                 <div class="flex flex-col gap-4">
                                     <span class="text-white">Please rate your experience with our Driver.</span>
                                     <div class="flex flex-row gap-1">
@@ -235,6 +240,7 @@
 
                                     </div>
                                 </div>
+                                @endif
                             </div>
 
                         </div>
@@ -242,10 +248,7 @@
                         <div
                             class="flex items-center p-4 md:p-5 space-x-3 rtl:space-x-reverse border-t border-gray-200 rounded-b dark:border-gray-600">
                             <button data-modal-hide="large-modal" type="button"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">I
-                                accept</button>
-                            <button data-modal-hide="large-modal" type="button"
-                                class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Decline</button>
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
                         </div>
                     </div>
                 </div>
@@ -280,7 +283,7 @@
         const starthree = document.getElementById(`starthree-${id}`);
         const starfour = document.getElementById(`starfour-${id}`);
         const starfive = document.getElementById(`starfive-${id}`);
-        const clear = document.getElementById('clear-rating');
+        const clear = document.getElementById(`clear-rating-${id}`);
     
         if (starone) {
             starone.addEventListener('click', function() {
@@ -372,7 +375,7 @@
         const starthree2 = document.getElementById(`starthree2-${id}`);
         const starfour2 = document.getElementById(`starfour2-${id}`);
         const starfive2 = document.getElementById(`starfive2-${id}`);
-        const clear = document.getElementById('clear-rating');
+        const clear = document.getElementById(`clear-rating-${id}`);
     
         if (starone2) {
             starone2.addEventListener('click', function() {
