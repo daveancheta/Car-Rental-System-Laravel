cf32dbdd0eef937cd071f267011454389bcea33a
Heaven Dave Ancheta <daveancheta8@gmail.com>
2025-08-07 18:51:02 +0800

    updated alignment of the account status anf verification

--- resources/views/admin/crud-user.blade.php
+++ resources/views/admin/crud-user.blade.php
@@ -68,41 +68,48 @@
                     </th>
                     <td class="px-10 py-4">
                         @if ($u->is_admin === NULL || $u->is_driver === NULL)
-                        <div
-                            class="dark:bg-indigo-100 dark:text-indigo-800 p-1 px-4 rounded-md flex flex-row items-center pl-8">
-                            <svg class="w-4 h-4 text-indigo-800 dark:text-indigo-800" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                                viewBox="0 0 24 24">
-                                <path fill-rule="evenodd"
-                                    d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
-                                    clip-rule="evenodd" />
-                            </svg>
+                        <div class="bg-indigo-100 p-1 px-4 rounded-md flex flex-row items-center justify-center">
+                            <div class="flex items-center">
+                                <svg class="w-4 h-4 text-indigo-800 dark:text-indigo-800" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                    viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd"
+                                        d="M12 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8Zm-2 9a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2v-1a4 4 0 0 0-4-4h-4Z"
+                                        clip-rule="evenodd" />
+                                </svg>
 
-                            <p class="text-xs">Customer</p>
+                                <span class="text-xs text-indigo-800">Customer</span>
+                            </div>
                         </div>
                         @elseif($u->is_admin === 1 || $u->is_driver === 0)
-                        <div class="bg-blue-100 p-1 px-4 rounded-md flex flex-row items-center">
-                            <svg class="w-4 h-4 text-blue-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-                                <path fill-rule="evenodd"
-                                    d="M5 8a4 4 0 1 1 7.796 1.263l-2.533 2.534A4 4 0 0 1 5 8Zm4.06 5H7a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h2.172a2.999 2.999 0 0 1-.114-1.588l.674-3.372a3 3 0 0 1 .82-1.533L9.06 13Zm9.032-5a2.907 2.907 0 0 0-2.056.852L9.967 14.92a1 1 0 0 0-.273.51l-.675 3.373a1 1 0 0 0 1.177 1.177l3.372-.675a1 1 0 0 0 .511-.273l6.07-6.07a2.91 2.91 0 0 0-.944-4.742A2.907 2.907 0 0 0 18.092 8Z"
-                                    clip-rule="evenodd" />
-                            </svg>
+                        <div class="bg-blue-100 p-1 px-4 rounded-md flex flex-row items-center justify-center">
+                            <div class="flex items-center">
+                                <svg class="w-4 h-4 text-blue-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                    width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd"
+                                        d="M5 8a4 4 0 1 1 7.796 1.263l-2.533 2.534A4 4 0 0 1 5 8Zm4.06 5H7a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h2.172a2.999 2.999 0 0 1-.114-1.588l.674-3.372a3 3 0 0 1 .82-1.533L9.06 13Zm9.032-5a2.907 2.907 0 0 0-2.056.852L9.967 14.92a1 1 0 0 0-.273.51l-.675 3.373a1 1 0 0 0 1.177 1.177l3.372-.675a1 1 0 0 0 .511-.273l6.07-6.07a2.91 2.91 0 0 0-.944-4.742A2.907 2.907 0 0 0 18.092 8Z"
+                                        clip-rule="evenodd" />
+                                </svg>
 
-                            <span class="text-xs text-blue-800">Administrator</span>
+                                <span class="text-xs text-blue-800">Administrator</span>
+                            </div>
+
                         </div>
                         @elseif($u->is_admin === 0 || $u->is_driver === 1)
-                        <div class="bg-yellow-100 p-1 px-4 rounded-md flex flex-row items-center pl-10">
-                            <svg class="w-4 h-4 text-yellow-800" aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                                viewBox="0 0 24 24">
-                                <path fill-rule="evenodd"
-                                    d="M17 10v1.126c.367.095.714.24 1.032.428l.796-.797 1.415 1.415-.797.796c.188.318.333.665.428 1.032H21v2h-1.126c-.095.367-.24.714-.428 1.032l.797.796-1.415 1.415-.796-.797a3.979 3.979 0 0 1-1.032.428V20h-2v-1.126a3.977 3.977 0 0 1-1.032-.428l-.796.797-1.415-1.415.797-.796A3.975 3.975 0 0 1 12.126 16H11v-2h1.126c.095-.367.24-.714.428-1.032l-.797-.796 1.415-1.415.796.797A3.977 3.977 0 0 1 15 11.126V10h2Zm.406 3.578.016.016c.354.358.574.85.578 1.392v.028a2 2 0 0 1-3.409 1.406l-.01-.012a2 2 0 0 1 2.826-2.83ZM5 8a4 4 0 1 1 7.938.703 7.029 7.029 0 0 0-3.235 3.235A4 4 0 0 1 5 8Zm4.29 5H7a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h6.101A6.979 6.979 0 0 1 9 15c0-.695.101-1.366.29-2Z"
-                                    clip-rule="evenodd" />
-                            </svg>
+                        <div class="bg-yellow-100 p-1 px-4 rounded-md flex flex-row items-center justify-center">
+                            <div class="flex items-center">
+                                <svg class="w-4 h-4 text-yellow-800" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                                    viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd"
+                                        d="M17 10v1.126c.367.095.714.24 1.032.428l.796-.797 1.415 1.415-.797.796c.188.318.333.665.428 1.032H21v2h-1.126c-.095.367-.24.714-.428 1.032l.797.796-1.415 1.415-.796-.797a3.979 3.979 0 0 1-1.032.428V20h-2v-1.126a3.977 3.977 0 0 1-1.032-.428l-.796.797-1.415-1.415.797-.796A3.975 3.975 0 0 1 12.126 16H11v-2h1.126c.095-.367.24-.714.428-1.032l-.797-.796 1.415-1.415.796.797A3.977 3.977 0 0 1 15 11.126V10h2Zm.406 3.578.016.016c.354.358.574.85.578 1.392v.028a2 2 0 0 1-3.409 1.406l-.01-.012a2 2 0 0 1 2.826-2.83ZM5 8a4 4 0 1 1 7.938.703 7.029 7.029 0 0 0-3.235 3.235A4 4 0 0 1 5 8Zm4.29 5H7a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h6.101A6.979 6.979 0 0 1 9 15c0-.695.101-1.366.29-2Z"
+                                        clip-rule="evenodd" />
+                                </svg>
 
 
-                            <span class="text-xs text-yellow-800">Driver</span>
+                                <span class="text-xs text-yellow-800">Driver</span>
+                            </div>
+
                         </div>
                         @endif
 
@@ -121,8 +128,9 @@
     </td>
     <td class="px-10 py-4 capitalize">
         @if($u->account_status === 'verified')
-        <div class="bg-green-100 p-1 px-4 rounded-md flex fle-row items-center">
-            <svg class="w-4 h-4 text-gray-800 dark:text-green-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+        <div class="bg-green-100 p-1 px-4 rounded-md flex fle-row items-center justify-center">
+            <div class="flex items-center">
+                <svg class="w-4 h-4 text-gray-800 dark:text-green-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                 width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                     d="M5 11.917 9.724 16.5 19 7.5" />
@@ -130,9 +138,12 @@
 
             <span class="text-green-800 text-xs">
                 {{ $u->account_status }}</span>
+            </div>
+            
         </div>
         @elseif($u->is_admin === 1 || $u->is_driver === 0)
-        <div class="bg-blue-100 p-1 px-4 rounded-md flex fle-row items-center">
+        <div class="bg-blue-100 p-1 px-4 rounded-md flex fle-row items-center justify-center">
+            <div class="flex items-center">
             <svg class="w-4 h-4 text-blue-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
                 height="24" fill="currentColor" viewBox="0 0 24 24">
                 <path fill-rule="evenodd"
@@ -141,9 +152,11 @@
             </svg>
 
             <span class="text-xs text-blue-800">Administrator</span>
+            </div>
         </div>
         @else
-        <div class="bg-red-100 p-1 px-4 rounded-md flex fle-row items-center">
+        <div class="bg-red-100 p-1 px-4 rounded-md flex fle-row items-center justify-center">
+            <div class="flex items-center">
             <svg class="w-4 h-4 text-red-800" fill="currentColor" viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 
@@ -154,6 +167,7 @@
          5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
             </svg>
             <span class="text-xs text-red-800">{{ $u->account_status ?? 'Unverified'}}</span>
+            </div>
         </div>
         @endif
 
